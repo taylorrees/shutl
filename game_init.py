9b79642ae413b9b45e294fe98bb5057942bb2408
Patricia Booth <patriciabooth89@yahoo.com>
2015-10-20 12:08:09 +0100

    Changed main description

--- game_init.py
+++ game_init.py
@@ -2,38 +2,43 @@
 game_description = '''
         GAME DESCRIPTION:
 
-        You wake up on the second floor of the Queens
-        building of Cardiff University in a blaze of
-        thick smoke as you attempt to gasp for air you
-        desperately need. You soon find out that you
-        had been knocked unconscious in the mist of a
-        raging fire. You must escape to save your life.
-        On your way out there will be essential items
-        which are needed to aid your attempt to live,
-        only the correct item will aid the correct exit
-        and save you from being engulfed by the fire or
-        dying due to being smothered by the thick black
-        smoke which is getting thicker and closer by
-        the second.
+        You wake up on the second floor of a tower 
+        building in a blaze of thick smoke. You attempt 
+        to gasp for air you desperately need. You soon 
+        find out that you had been knocked unconscious 
+        in the mist of a raging fire. You must escape 
+        to save your life. On your way out there will
+        be essential items which are needed to aid 
+        your attempt to live. Only the correct items 
+        will lead you to the exit to save you from being 
+        engulfed by the fire. Otherwise you may die due 
+        to being smothered by the thick black smoke which 
+        is getting thicker and closer by the second.
 
-        You are able to move left and right then up a
-        floor or down a floor. There is a door to get out
-        on the bottom floor which will allow you to escape
-        and breathe the air you badly need, a fire exit
-        towards the left hand side of the building, although
-        you are agonisingly close to the ground floor but
-        not close enough to jump without dying. Also, on the
-        roof is a helicopter, the helicopter has come to save
-        you but you may find a few obstacles on the way.
+        You are able to move left and right then up and
+        down floors. There is a door located in the building 
+        which will allow you to escape. There is a fire exit 
+        towards the left hand side of the building. You are 
+        agonisingly close to the ground floor but not close 
+        enough to jump without dying. Also, on the roof is a 
+        helicopter, the helicopter has come to save you but 
+        you may find a few obstacles 
+        on the way.
 
-        Gaining the items along the way will help you no
-        end, you must think wisely to which exit will be
-        the easiest and quickest as your health will slowly
-        be cascading due to the venomous fire and the dangerous
-        smoke. The more moves you take, the more health you
-        will lose, for example, a health draining climb up
-        the steep steps of stairs will steal 5 health points.
-        Chose your moves wisely.
+        Finding the items throughout the building will help 
+        you overcome the obstacles along the way. There is 
+        a chocolate bar that you may pick up along your path, 
+        if your health is below 50 it will give you 50 bonus 
+        points otherwise if your above 50 then it regenerates
+        your health back to 100. You must think wisely to 
+        which exit will be the easiest and quickest as your 
+        health will slowly be decreasing due to the venomous 
+        fire and the dangerous smoke. The more moves you take, 
+        the more health you may lose, for example, moving from 
+        room to room will differ from how many points it will 
+        take from your health.
+
+        Choose your moves wisely.
 '''
 
 visual_map = '''
