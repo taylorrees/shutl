5d31039430927b7c7b9675783f63514ad73a63e3
Patricia Booth <patriciabooth89@yahoo.com>
2015-10-20 11:15:25 +0100

    Changed description

--- map.py
+++ map.py
@@ -298,10 +298,11 @@ first_fire_escape = {
     """,
 
     """
-    You have attached the rope to the fire
-    escape to climb down and used the brick to smash the window. You have successfully attained the items
-    which will allow you to cascade down to the ground
-    floor. Your inventory has now been cleared.
+    You have successfully attained the items. You have 
+    attached the rope to the fire escape to climb down 
+    and used the brick to smash the window. This will 
+    allow you to cascade down to the ground floor. Your 
+    inventory has now been cleared.
     """
 
     ],
